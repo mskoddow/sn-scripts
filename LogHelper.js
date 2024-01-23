659c751ddfbc838848113b56c450351cd83472fb
mskoddow <58812581+mskoddow@users.noreply.github.com>
2024-01-23 04:42:58 +0100

    Update LogHelper.js
    Optimized version

--- LogHelper.js
+++ LogHelper.js
@@ -1,12 +1,13 @@
 var LogHelper = Class.create();
 
 /**
- * Server side helper class for logging purposes. This class supports a debug mode. 
- *
- * @class LogHelper
- * @author Maik Skoddow
- * @see https://www.servicenow.com/community/developer-articles/just-another-log-helper/ta-p/2315453
- */
+* Server side helper class for logging purposes. This class supports a debug mode. 
+* If enabled via system property or directly debug log statements are written to Sys Log.
+*
+* @class LogHelper
+* @author Maik Skoddow
+* @see https://www.servicenow.com/community/developer-articles/just-another-log-helper/ta-p/2315453
+*/
 LogHelper.prototype = {
 	type: 'LogHelper'
 };
@@ -18,13 +19,13 @@ LogHelper.DEBUG_PROPERTY = 'loghelper.enable.debug';
 LogHelper._source = "ACME";
 
 
-/**
- * Writes a log message to system log of type 'info' and with a given scope in case debug mode is enabled.
- */
+/*
+* Writes a log message to system log of type 'info' and with a given scope in case debug mode is enabled.
+*/
 LogHelper.debug = function(strScope, strMessage) {
 	var _strDebugProperty = gs.getProperty(LogHelper.DEBUG_PROPERTY, "false");
 	
-	if (_strDebugProperty && _strDebugProperty === "true") {
+	if (_strDebugProperty && _strDebugProperty == "true") {
 		var _strMessage = LogHelper._getMessageStr(arguments);
 
 		LogHelper._insertSysLogRow('-1', _strMessage);
@@ -36,9 +37,9 @@ LogHelper.debug = function(strScope, strMessage) {
 };
 
 
-/**
- * Writes a log message to system log of type 'info' and with a given scope.
- */
+/*
+* Writes a log message to system log of type 'info' and with a given scope.
+*/
 LogHelper.info = function(strScope, strMessage) {
 	var _strMessage = LogHelper._getMessageStr(arguments);
 
@@ -48,9 +49,9 @@ LogHelper.info = function(strScope, strMessage) {
 };
 
 
-/**
- * Writes a log message to system log of type 'warn' and with a given scope.
- */
+/*
+* Writes a log message to system log of type 'warn' and with a given scope.
+*/
 LogHelper.warn = function(strScope, strMessage) {
 	var _strMessage = LogHelper._getMessageStr(arguments);
 
@@ -61,8 +62,8 @@ LogHelper.warn = function(strScope, strMessage) {
 
 
 /*
- * Writes a log message to system log of type 'error' and with a given scope.
- */
+* Writes a log message to system log of type 'error' and with a given scope.
+*/
 LogHelper.error = function(strScope, strMessage) {
 	var _strMessage = LogHelper._getMessageStr(arguments);
 
@@ -71,10 +72,10 @@ LogHelper.error = function(strScope, strMessage) {
 	return _strMessage;
 };
 
-/**
+/*
  * Writes a log message to system log of type 'error' and with a given scope. Addtionally 
  * provided exception {e} is written to system log.
- */
+*/
 LogHelper.fatal = function(strScope, strMessage, e) {
 	var _strMessage = LogHelper._getMessageStr(arguments);
 
@@ -88,18 +89,18 @@ LogHelper._getMessageStr = function(arrArguments) {
 	var _args = [];
 	var _e;
   
-	for (var _numIndex1 = 0; _numIndex1 < arrArguments.length; _numIndex1++) {
-		if (arrArguments[_numIndex1] !== null && arrArguments[_numIndex1] !== 'undefined') {
-			if (arrArguments[_numIndex1] instanceof Error) {
-				_e = arrArguments[_numIndex1];
+	for (var numIndex = 0; numIndex < arrArguments.length; numIndex++) {
+		if (arrArguments[numIndex] !== null && arrArguments[numIndex] !== 'undefined') {
+			if (arrArguments[numIndex] instanceof Error) {
+				_e = arrArguments[numIndex];
 			}
 			else {
-				_args.push(arrArguments[_numIndex1]);
+				_args.push(arrArguments[numIndex]);
 			}
 		}
 	}
   
-	var _strFullNodeName   = GlideServlet.getSystemID();
+	var _strFullNodeName   = global.GlideServlet.getSystemID();
 	var _numSeparatorIndex = _strFullNodeName.indexOf(':');
 	
 	var _strShortNodeName  = 
@@ -114,16 +115,26 @@ LogHelper._getMessageStr = function(arrArguments) {
 		(_args.length < 2 ? '???' : _args[1].toString());
   
   
-	for (var _numIndex2 = 2; _numIndex2 < _args.length; _numIndex2++) {
-		var _strValue = JSUtil.nil(_args[_numIndex2]) ? '' : _args[_numIndex2].toString();
+	for (var _numIndex = 2; _numIndex < _args.length; _numIndex++) {
+		var _strValue = JSUtil.nil(_args[_numIndex]) ? '' : _args[_numIndex].toString();
 		
-		_strMessage = _strMessage.replace(new RegExp('{' + (_numIndex2 - 2) + '}', 'g'), _strValue);
+		_strMessage = _strMessage.replace(new RegExp('{' + (_numIndex - 2) + '}', 'g'), _strValue);
 	}
   
 	if (_e) {
 		_strMessage = 
 			_strMessage + '\n--> ' + 
-			_e.message.substring(0, _e.message.length - 1) + ' in ' + _e.fileName + ' at line ' + _e.lineNumber;
+			_e.message + '\n' + 
+			_e.stack
+				.split('\n')
+				.slice(0, 4)
+				.filter(function(line) {
+					return line.trim().length > 0;
+				})
+				.map(function (line) {
+					return '----> ' + line.trim();
+				})
+				.join('\n');
 	}
 
 	return _strMessage;
